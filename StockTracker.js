e08d3ca96db469552359b1c289697f9b07adc773
willinfante <42078563+willinfante@users.noreply.github.com>
2019-08-25 07:55:32 -0400

    Update StockTracker.js
    Moved the data request into the 'getData' function

--- StockTracker.js
+++ StockTracker.js
@@ -15,49 +15,50 @@ function isPositive(num) {
   return num >= 0;
 }
 
-function getData(num, dat, symb, callback){
+function getData(num, symb, callback){
+	https.get(command + symb,(resp) =>{
 
-    var symbol = JSON.parse(dat).quoteResponse.result[num].symbol;
-    var short = JSON.parse(dat).quoteResponse.result[num].longName;
-    var price = JSON.parse(dat).quoteResponse.result[num].regularMarketPrice;
-    var open = JSON.parse(dat).quoteResponse.result[num].regularMarketOpen;
-    var high = JSON.parse(dat).quoteResponse.result[num].regularMarketDayHigh;
-    var low = JSON.parse(dat).quoteResponse.result[num].regularMarketDayLow;
-    var prevClose =  JSON.parse(dat).quoteResponse.result[num].regularMarketPreviousClose;
-    var ask = JSON.parse(dat).quoteResponse.result[num].bid;
-    var pe = JSON.parse(dat).quoteResponse.result[num].trailingPE;
-    var mktcp = JSON.parse(dat).quoteResponse.result[num].marketCap;
-    var regmktch = JSON.parse(dat).quoteResponse.result[num].regularMarketChange;
-    var mktvol = JSON.parse(dat).quoteResponse.result[num].regularMarketVolume;
-    var state = JSON.parse(dat).quoteResponse.result[num].regularMarketState;
-    var bid = JSON.parse(dat).quoteResponse.result[num].bid;
-    var ask = JSON.parse(dat).quoteResponse.result[num].ask;
-    var divps =JSON.parse(dat).quoteResponse.result[num].dividendsPerShare;
-    var rev = JSON.parse(dat).quoteResponse.result[num].revenue;
-    var sO = JSON.parse(dat).quoteResponse.result[num].sharesOutstanding;
-    var tradable = JSON.parse(dat).quoteResponse.result[num].tradable;
-    var ftwhcp =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHighChangePercent;
-    var ftwl =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLow;
-    var ftwh =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHigh;
-    var ftwhc =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHighChange;
-    var ftwlc =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLowChange;
-    var ftwlcp =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLowChangePercent;
+	   let data = '';
 
-    var result = callback(symbol, short, price, open, high, low, prevClose, ask, pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp);
+	    // A chunk of data has been recieved.
+	    resp.on('data', (chunk) => {
+		data += chunk;
+	    });
+		
+	    resp.on('end', () => {
+		    var symbol = JSON.parse(dat).quoteResponse.result[num].symbol;
+		    var short = JSON.parse(dat).quoteResponse.result[num].longName;
+		    var price = JSON.parse(dat).quoteResponse.result[num].regularMarketPrice;
+		    var open = JSON.parse(dat).quoteResponse.result[num].regularMarketOpen;
+		    var high = JSON.parse(dat).quoteResponse.result[num].regularMarketDayHigh;
+		    var low = JSON.parse(dat).quoteResponse.result[num].regularMarketDayLow;
+		    var prevClose =  JSON.parse(dat).quoteResponse.result[num].regularMarketPreviousClose;
+		    var ask = JSON.parse(dat).quoteResponse.result[num].bid;
+		    var pe = JSON.parse(dat).quoteResponse.result[num].trailingPE;
+		    var mktcp = JSON.parse(dat).quoteResponse.result[num].marketCap;
+		    var regmktch = JSON.parse(dat).quoteResponse.result[num].regularMarketChange;
+		    var mktvol = JSON.parse(dat).quoteResponse.result[num].regularMarketVolume;
+		    var state = JSON.parse(dat).quoteResponse.result[num].regularMarketState;
+		    var bid = JSON.parse(dat).quoteResponse.result[num].bid;
+		    var ask = JSON.parse(dat).quoteResponse.result[num].ask;
+		    var divps =JSON.parse(dat).quoteResponse.result[num].dividendsPerShare;
+		    var rev = JSON.parse(dat).quoteResponse.result[num].revenue;
+		    var sO = JSON.parse(dat).quoteResponse.result[num].sharesOutstanding;
+		    var tradable = JSON.parse(dat).quoteResponse.result[num].tradable;
+		    var ftwhcp =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHighChangePercent;
+		    var ftwl =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLow;
+		    var ftwh =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHigh;
+		    var ftwhc =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekHighChange;
+		    var ftwlc =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLowChange;
+		    var ftwlcp =  JSON.parse(dat).quoteResponse.result[num].fiftyTwoWeekLowChangePercent;
 
+		    var result = callback(symbol, short, price, open, high, low, prevClose, ask, pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp);		    
+	    });	
+	});
 }
 
-https.get(command + "TSLA",(resp) =>{
 
-           let data = '';
-
-           // A chunk of data has been recieved.
-          resp.on('data', (chunk) => {
-                data += chunk;
-          });
-         resp.on('end', () => {
          getData(0, data, function(symbol, short, price, open, high, low, prevClose, ask, pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp) {
-		  console.log("yeet");
 
          	  if(conout == true) {
 			  console.log("STOCK REPORT - " + symbol + "(" + short + ")");
@@ -102,8 +103,6 @@ https.get(command + "TSLA",(resp) =>{
 			});
 		 }
               });
-        });
-   });
 
 
 
