adfc8fe8d525bfe4f577320441679bc10b85b25f
willinfante <42078563+willinfante@users.noreply.github.com>
2019-08-29 13:10:20 -0400

    Update StockTracker.js
    Comments

--- StockTracker.js
+++ StockTracker.js
@@ -1,43 +1,32 @@
 var exec = require('child_process').exec; //Used to Print the output to a network printer
 var https = require('https');  //Used to Make Requests to yahoo finance
 var colors = require('ansi-256-colors'); //Used to add color to text printed in console
-var fs = require('fs');
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+var fs = require('fs'); //Used for the share value system
 
 //The API we are making requests to
 var command = "https:\/\/query1.finance.yahoo.com/v7/finance/quote?fields=symbol,longName,shortName,regularMarketPrice,regularMarketTime,regularMarketChange,regularMarketDayHigh,regularMarketDayLow,regularMarketPrice,regularMarketOpen,regularMarketVolume,averageDailyVolume3Month,marketCap,bid,ask,dividendYield,dividendsPerShare,exDividendDate,trailingPE,priceToSales,tarketPricecMean&formatted=false&symbols="
 var express = require('express'); //Used to create the Server
 var app = express(); 
 var conout = false;  //The variable that decides wheather to do the server or print to the console
-var conoutm = false;
-var printout = false;
-var servero = false;
-var eEntry = false;
-var helpo = false;
+var conoutm = false; //Print multiple stocks to console
+var printout = false; //Print output to network printer
+var servero = false; //Set up Server
+var eEntry = false; //Create Share Entry
+var helpo = false; //Help
 if(process.argv[2] == "-c") conout = true; //If the command entered doesn't have the paramaters needed to print to the console, don't print to the console
 
-if(process.argv[2] == "-m") conoutm = true;
+if(process.argv[2] == "-m") conoutm = true; //If We put -m as an argument, print out multiple stocks
 
-if(process.argv[2] == "-p") printout = true;
+if(process.argv[2] == "-p") printout = true;//If We put -p as an argument, print to printer
 
-if(process.argv[2] == "-s") servero = true;
+if(process.argv[2] == "-s") servero = true;//If We put -s as an argument, start server
+if(process.argv[2] == "-h") helpo = true;//If We put -h as an argument, print help
 
-if(process.argv[2] == "-h") helpo = true;
+if(process.argv[2] == "-e") eEntry = true;//If We put -e as an argument, create entry
 
-if(process.argv[2] == "-e") eEntry = true;
-//////Going to be used for colors later
-//function isPositive(num) {
-//  return num >= 0;
-//}
-//////////////////////////////////////
-
+//If the user wants help...
 function help(){
-
+	//Print Out Help Info
 	console.log("To Run The Server" + "\n" + "pi@raspberry:~/StockTracker$ node StockTracker.js -s\n\n" + "Go to http://[YOUR-IP]:8080/ and enter a stock symbol\n");
 	console.log("To Print to the Console\n" + "pi@raspberry:~/StockTracker$ node StockTracker.js -c [INSERT STOCK SYMBOL HERE]\n");
 	console.log("To Watch Live in the console\n" + "pi@raspberry:~/StockTracker$ watch node StockTracker.js -c [INSERT STOCK SYMBOL]\n");
@@ -48,18 +37,26 @@ function help(){
 
 }
 
+//Stock Infromation Printer
 function printstock(s, p, r, d) {
+	
+	//Read File that stores # of shares
 	fs.readFile('/home/pi/Documents/StockTracker/ShareData/' + s +'.txt', (err, data) => {
+		
+		//If there is no file... notify the user
 		if(!data)  console.log("\x1b[41m%s\x1b[0m", "You do not have an entry for the number of shares you own in this stock. Make one by using -e(-h for more info on parameters");
-  		if(data) var vall = parseInt(p) * parseInt(data);
-
+  		
+		//If there is.. create the value
+		if(data) var vall = parseInt(p) * parseInt(data);
+		
+		//Print out the data
 		if(data) console.log(s + "       " + p + "         " + r + "          " + vall.toString() + "\n");
                 if(!data) console.log(s + "       " + p + "         " + r + "\n");
 
 	});
 }
 
-
+//If the user wants a server
 function serve(){
 
 	app.listen(8080); //Start Listening For Request On Port 8080
@@ -87,33 +84,52 @@ function serve(){
 	});
 }
 
+//If the user wants to print out multiple stocks...
 function conoutmult() {
-	var array = [];
-	console.log("STOCK       PRICE       CHANGE      VALUE");
-        for (var i = 3; i < process.argv.length; i++){
-		array.push(process.argv[i]);
+	
+	var array = []; //Create an array for stocks to print based on arguments
+	console.log("STOCK       PRICE       CHANGE      VALUE"); //Print out the key
+     
+	for (var i = 3; i < process.argv.length; i++){ //Loop through arguments
+		array.push(process.argv[i]); //add the arguments to the array
 	}
-	for(var symb of array){
+	
+	for(var symb of array){ //go through each element in the array
+		
 		//Get the data from the parameters in the console
 		getData(0, symb, function(symbol, short, price, open, high, low, prevClose, pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp) {
-
+			  
+			    //Print out the information
 			    printstock(symbol, price, regmktch,	divps);
 		});
 	}
 }
 
+//If the user wants to make a share number entry
 function createEntry(stock, shareNum){
 
+     //Write the file
      fs.writeFileSync('ShareData/' + stock + '.txt', shareNum, 'utf8');
+
+     //Kill the process
      process.exit();
 
 }
-function pout() {
-	getData(0, process.argv[4].toString(), function(symbol, short, price, open, high, low, prevClose,  pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp) {
-		fs.readFile('/home/pi/Documents/StockTracker/ShareData/' + process.argv[4].toString() +'.txt', (err, data) => {
 
+//If the user wants to print to a network printer
+function pout() {
+	
+	//Get the data
+	getData(0, process.argv[4].toString(), function(symbol, short, price, open, high, low, prevClose,  pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp) {
+		
+		//Get the number of shares
+		fs.readFile('/home/pi/Documents/StockTracker/ShareData/' + process.argv[4].toString() +'.txt', (err, data) => {
+			
+			  //If there is no file... notify the user
 			  if(!data) console.log("\x1b[41m%s\x1b[0m","You have not made an entry for this stock. Create one using -e [STOCK] [SHARES], or do -f if you don't 0wn any.");
-                          if(data) var vall = parseInt(price)  * parseInt(data);
+                       
+			  //If there is.. find the value
+			  if(data) var vall = parseInt(price)  * parseInt(data);
 			  var out = "";
 
 			  //Print all the information to the console
@@ -141,9 +157,16 @@ function pout() {
                           out += "FIFTY-TWO WEEK LOW CHANGE PERCENT : " + ftwlcp + "\n";
 			  if(data) out += "YOUR SHARE VALUE : " + vall;
 
+			  //Write a temporary file with the data
 			  fs.writeFileSync('o.txt', out, 'utf8');
+			
+			   //Send it to the printer
 			  exec('cat o.txt | telnet ' + process.argv[3].toString() + ' 9100', (err,stdout,stderr) => {
+				  
+				  //Delete the file
 				  fs.unlinkSync('o.txt');
+	
+				  //Kill the process
 				  process.exit();
 			  });
 		});
@@ -153,14 +176,18 @@ function pout() {
 
 }
 function conoutf() {
+	
 	//Get the data from the parameters in the console
-
 	 getData(0, process.argv[3].toString(), function(symbol, short, price, open, high, low, prevClose,  pe, mktcp, regmktch, mktvol, state, bid, ask, divps, rev, sO, tradable, ftwhcp, ftwl, ftwh, ftwhc, ftwlc, ftwlcp) {
-
+	
+		//Get the number of shares
 		fs.readFile('/home/pi/Documents/StockTracker/ShareData/' + process.argv[3].toString() +'.txt', (err, data) => {
 
+			  //If there is no file... notify the user
 			  if(!data) console.log("\x1b[41m%s\x1b[0m", "You have no entry for the number of shares you 0wn in this company. Make one by using -e(-h for more info)");
-			 if(data){
+			  
+			  //If there is.. find the value
+			  if(data){
 			  	var vall = parseInt(price)  * parseInt(data);
 			  }
 			  //Print all the information to the console
